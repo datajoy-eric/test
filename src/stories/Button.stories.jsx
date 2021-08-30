fb7923b2bcecd8cef4b7786668424aeaf6618fa6
Eric Alas <eric@datajoy.com>
2021-08-30 12:31:28 -0400

    t7

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -15,7 +15,7 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button ERIC ALAS TESTING AGAOIN',
+  label: 'Eric Hello',
 };
 
 export const Secondary = Template.bind({});
